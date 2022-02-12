091210c59772185cc3f16a84247fd5131ea5ec01
Duck commander <vespall2910@gmail.com>
2022-02-12 15:24:07 +0300

    Незначительные изменения

--- ProverkaChislaNaChetnost/Program.cs
+++ ProverkaChislaNaChetnost/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProverkaChislaNaChetnost
 {
@@ -15,7 +11,7 @@ namespace ProverkaChislaNaChetnost
             Console.WriteLine("Введите число");
             num = int.Parse(Console.ReadLine());
 
-            if (num % 2 == 1 )
+            if (num % 2 == 1)
             {
                 Console.WriteLine("Это число не является чётным");
             }
