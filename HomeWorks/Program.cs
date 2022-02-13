a223099fd7869943e2451b4fe43e2823c0b7e828
Duck_commander <vespall2910@gmail.com>
2022-02-13 14:53:57 +0300

    Проверка git

--- HomeWorks/Program.cs
+++ HomeWorks/Program.cs
@@ -10,6 +10,25 @@ namespace HomeWorks
     {
         static void Main(string[] args)
         {
+            int b = 5;
+            string a = "45,6";
+
+            Console.WriteLine(a + b); // = 45,65
+            Console.WriteLine(b + a); // = 545,6
+            Console.WriteLine(b + float.Parse(a)); // = 50,6
+
+            a = Console.ReadLine();
+            b = 15;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(b + a);
+            Console.WriteLine(b + int.Parse(a));
+
+            int c = 10;
+            b = 5;
+
+            Console.WriteLine(c / b); // 2
+            Console.WriteLine(Convert.ToSingle(b) / c); // 0,5
 
         }
     }
---
+++ KratnostTrem/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KratnostTrem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num; // присваеваем переменной num целочисленный тип int
+
+
+            Console.WriteLine("Введите любое число"); // выводим в консоль текст 
+            num = int.Parse(Console.ReadLine()); // 
+            if (num % 3 == 0) // если остаток от деления введенного пользователем числа на три равен нулю
+            {
+                if (num == 0) 
+                {
+                    Console.WriteLine("ЕРРЕР");
+                }
+                else
+                {
+                    Console.WriteLine("Это число кратно трём");
+                }
+                if (num < 0)
+                {
+                    Console.WriteLine("Нет, впиши положительное число");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Это число не кратно трём");
+            }
+        }
+    }
+}
