667161f6e7e1165e1d57f017c9a6059790b184eb
Duck_commander <vespall2910@gmail.com>
2022-02-22 21:23:55 +0300

    add some Love

---
+++ Love/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Love
+{
+    internal class Program
+    {
+
+        public static bool Color(double weight, double height, double maxweight)
+        {
+            if ((weight == (maxweight / 4 + 0.5) || weight == (maxweight * 3 / 4 + 0.5)) && height <= maxweight - ((maxweight / 2 - 1) / 2))
+            {
+                return true;
+            }
+            if (weight > (maxweight / 4 + 0.5) - height && weight < (maxweight / 4 + 0.5) + height && height <= (maxweight / 2 - 1) / 2)
+            {
+                return true;
+            }
+            if (weight > (maxweight * 3 / 4 + 0.5) - height && weight < (maxweight * 3 / 4 + 0.5) + height && height <= (maxweight / 2 - 1) / 2)
+            {
+                return true;
+            }
+            if (height > (maxweight / 2 - 1) / 2 && height <= ((maxweight / 2 - 1) / 2) + (maxweight / 2 - 2))
+            {
+                return true;
+            }
+            if (height > (maxweight / 2 - 1) / 2 + (maxweight / 2 - 2) && (weight > ((height + (((maxweight / 2 - 1) / 2 + (maxweight / 2 - 2)) % 1)) - ((maxweight / 2 - 1) / 2 + (maxweight / 2 - 2)))) && weight <= (maxweight - (height - ((maxweight / 2 - 1) / 2 + (maxweight / 2 - 2)))) )
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Любооооооооооооооофь";
+
+            double height, weight;
+
+            while (true)
+            {
+                Console.Clear();
+
+                try
+                {
+                    Console.Write("Задайте ширину квадрата(советую от 7 и больше): ");
+                    weight = int.Parse(Console.ReadLine());
+                    height = weight;
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+                
+                for (int i = 1; i <= height; i++)
+                {
+                    for (int j = 1; j <= weight; j++)
+                    {
+                        if (Color(j, i, weight))
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                        }
+                        Console.Write("  ");
+                    }
+
+                    Console.WriteLine();
+                }
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Love you <3");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadLine();
+            }
+            
+
+            
+        }
+    }
+}
---
+++ Love/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Love")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Love")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8d4d6861-f7db-4f0d-bf93-a1923d2a9031")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
